faf46c3183bd97d4cac51608209436a68b9339ca
Thelight <condroxyedo@gmail.com>
2026-07-01 11:47:53 +0200

    Aggiornamento mod

---
+++ github update.bat
@@ -0,0 +1,20 @@
+@echo off
+:: Naviga automaticamente nella cartella della mod
+cd /d "C:\Users\condr\Documents\Paradox Interactive\Hearts of Iron IV\mod\3435731114"
+
+echo Seleziono i file modificati...
+git add .
+
+:: Ti chiede cosa hai cambiato (premi Invio per lasciare il messaggio predefinito)
+set /p msg="Inserisci il messaggio di commit (oppure premi Invio per lasciare 'Aggiornamento mod'): "
+if "%msg%"=="" set msg=Aggiornamento mod
+
+echo Salvo le modifiche locali...
+git commit -m "%msg%"
+
+echo Invio i file su GitHub...
+git push origin main
+
+echo.
+echo Caricamento completato con successo!
+pause
